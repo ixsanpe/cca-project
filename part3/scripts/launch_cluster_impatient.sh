9828796cb70f80c7c6804c0d1405cd95a533461e
HeathArthurLouis <alhpersonal@protonmail.com>
2022-05-09 21:52:29 +0200

    Added results of static scheduler

---
+++ part3/scripts/launch_cluster_impatient.sh
@@ -0,0 +1,9 @@
+export KOPS_STATE_STORE=gs://cca-eth-2022-group-25-impatient-lheath/
+export KOPS_FEATURE_FLAGS=AlphaAllowGCE
+PROJECT=`gcloud config get-value project`
+kops create -f ../part_3_impatient.yaml
+
+kops update cluster part3i.k8s.local --yes --admin
+kops validate cluster --wait 10m
+kubectl get nodes -o wide
+
